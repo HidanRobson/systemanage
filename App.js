5ad506ca2e4b5ac11bbcb647c4b1f30727275a11
HidanRobson <56197164+HidanRobson@users.noreply.github.com>
2023-03-05 11:53:23 -0300

    update

--- App.js
+++ App.js
@@ -2,15 +2,26 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import "react-native-gesture-handler";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+// const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+    //     <Stack.Screen
+    //       name="Inicio"
+    //       component={
+    //       }
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
   );
 }
